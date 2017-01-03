1c470be69d63edfda885fd91fd82f03091fcd0dc
Elliot Kauffmann <elliot.kauffmann@etu.unicce.fr>
2017-01-03 18:04:36 +0100

    Gestion des labels / branches

--- InterpreteurAsm/interpreter.c
+++ InterpreteurAsm/interpreter.c
@@ -8,6 +8,10 @@
 #define MAX_ARGS 3
 #define MAX_SIZE_ARGS 5
 
+typedef struct Label {
+	char name[50];
+	int line;
+};
 
 char *DATA_PROCESS[16] = { "AND", "EOR", "LSL", "LSR", "ASR", "ADC", "SBC", "ROR", "TST", "RSB", "CMP", "CMN", "ORR", "MUL", "BIC", "MVN" };
 char *SASM[6] = { "LSL", "LSR", "ASR", "ADD", "SUB", "MOV" };
@@ -22,7 +26,6 @@ void print_binary(int n) {
 		else
 			printf("0");
 	}
-
 }
 
 void decode_line(char *line, char(*instruction)[4], char(*args)[MAX_ARGS][MAX_SIZE_ARGS], int *nb_args) {
@@ -52,27 +55,53 @@ void interprete(char *src_path, char *dst_path) {
 	src = fopen(src_path, "r");
 	dst = fopen(dst_path, "w");
 	if (src == NULL) {
-		printf("FATAL ERROR: cannot open source file.");
+		printf("FATAL ERROR: cannot open source file.\n");
 		exit(1);
 	}
 	else if (dst == NULL) {
-		printf("FATAL ERROR: cannot open destination file.");
-		exit(1);
+		printf("FATAL ERROR: cannot open destination file.\n");
+		exit(1); 
 	}
 	assert(src != NULL && dst != NULL);
 
-	// Start translating
 	char str[MAX_SIZE];
-	while (fgets(str, sizeof(str), src)) {
+	int lblCount = 0;
+	struct Label labels[256];
+	int lineCount = 0;
+
+	// Parse once to save label positions and names
+	while (fgets(str, MAX_SIZE, src) && lineCount < 256) {
+		lineCount++;
+		int tknCount = 0;
+		if (!strchr(str, ' ')) {
+			strcpy(labels[lblCount].name, strtok(str, "\n"));
+			labels[lblCount].line = lineCount;
+			lblCount++;
+		}
+	}
+
+	// Close and open the file to parse again
+	if (fclose(src)) {
+		printf("Error: could not close source file.\n");
+	}
+	src = fopen(src_path, "r");
+	if (src == NULL) {
+		printf("FATAL ERROR: cannot open source file.\n");
+		exit(1);
+	}
+
+	// Start translating
+	while (fgets(str, MAX_SIZE, src)) {
 		char *line;
 		char args[MAX_ARGS][MAX_SIZE_ARGS];
 		int nb_args = 0;
 		int code_instr;
-		int start_line = 1;
 		for (line = strtok(str, "\n"); line; line = strtok(NULL, "\n")) {
 			printf("%s\n", line);
 			char instruction[4];
 			decode_line(line, &instruction, &args, &nb_args);
+			if (!nb_args)
+				continue;
 			if ((code_instr = search(instruction, DATA_PROCESS, 16)) != -1 && nb_args == 2) {
 				int arg1 = atoi(args[0]);
 				int arg2 = atoi(args[1]);
@@ -98,30 +127,35 @@ void interprete(char *src_path, char *dst_path) {
 				}
 			}
 			else if ((code_instr = search(instruction, LS, 2)) != -1) {
-			        int arg1 = atoi(args[0]);
+			    int arg1 = atoi(args[0]);
 				int arg2 = atoi(args[1]);
 				int imm = atoi(args[2]);
 				output = (0b011 << 13) + (code_instr << 11) + (imm << 6) + (arg2 << 3) + arg1;
 			}
 			else if ((code_instr = search(instruction, BR, 14)) != -1) {
-
-				// Look for the label, then compute its offset
-				// from the branch, passing it as an immediate
-				// value.
-			  
-			  int imm = 0;
-			  output = (0b1101 << 12) + (code_instr << 8) + imm;
+				int imm = 0;
+				for (int i = 0; i < lblCount; i++) {
+					if (strcmp(labels[i].name, args[0])) {
+						imm = labels[i].line;
+						break;
+					}
+				}
+				output = (0b1101 << 12) + (code_instr << 8) + imm;
 			}
-			start_line++;
 			char s_output[4];
 			print_binary(output);
 			putchar('\n');
 			sprintf(s_output, "%x", output);
 			fputs(s_output, dst);
+			printf("\n");
 		}
-
 	}
-
+	if (fclose(src)) {
+		printf("Error: could not close source file.\n");
+	}
+	else if (fclose(dst)) {
+		printf("Error: could not close destination file.\n");
+	}
 }
 
 void print_usage() {
